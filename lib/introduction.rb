f3e00e458c460353e4bbfece069caa1a27110d8c
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-21 16:48:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,8 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+name = "Dan"
+language = "Ember.js.\n"
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
